a660edc403eb31f9aff8e85a4745e45f6a44aaef
Jaela Wesley <jcwesley93@gmail.com>
2019-02-19 10:36:08 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+    languages.each do |style, language|
+        language.each do |lang, attribute|
+          if new_hash.keys.include?(lang)
+            new_hash[lang][:style] << style
+          else type_value_arr = :type.values
+            type_value = type_value_arr[0]
+            new_hash[lang] = {:type => type_value, :style => [style]}
+
+          end
+        end
+    end
+    return new_hash
+  end
+
+  new_hash = {}
+languages.each do |key, value|
+  value.each do |lang, type|
+    new_hash[lang] ||= type
+        new_hash[lang][:style] ||= []
+       new_hash[lang][:style] << key
+  end
 end
+new_hash
+
+end 
