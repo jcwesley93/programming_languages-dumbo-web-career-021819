5580c1c2c00438bafa67e4d31aff978225b15b20
Jaela Wesley <jcwesley93@gmail.com>
2019-02-19 10:38:55 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,7 @@ def reformat_languages(languages)
   new_hash = {}
     languages.each do |style, language|
         language.each do |lang, attribute|
-    new_hash[lang] ||= type
+    new_hash[lang] ||= attribute
         new_hash[lang][:style] ||= []
        new_hash[lang][:style] << key
   end
